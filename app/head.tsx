dfc1383d05ecae6c837fe1730b795f211d321420
Tej Mehul Parmar <64017555+TejParmar10@users.noreply.github.com>
2025-05-27 19:43:21 +0530

    added app

---
+++ app/head.tsx
@@ -0,0 +1,15 @@
+export default function Head() {
+  return (
+    <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="I'm Tej Parmar, Software Engineer from Mumbai, India." />
+      <meta name="keywords" content="Tej Parmar, portfolio, deep, software engineer dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
+      <meta property="og:site_name" content="Tej Parmar's Portfolio" />
+      <meta property="og:title" content="Portfolio | Tej Parmar - Software Engineer" />
+      <meta property="og:description"
+        content="I'm Tej Parmar, Software Engineer from Mumbai" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>Portfolio | Tej Parmar - Software Engineer</title>
+    </>
+  )
+}
